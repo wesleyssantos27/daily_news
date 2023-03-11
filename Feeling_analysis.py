b2de8561bc878dc474a8ae28e06d48025b0d2ddc
Wesley Santos <wesleyssantos27@gmail.com>
2023-03-10 22:08:50 -0300

    Adicao do schedule utilizando o crontab, correcoes nos codigos e criacao de diretorio

--- Feeling_analysis.py
+++ Feeling_analysis.py
@@ -3,6 +3,8 @@ import json
 import pandas as pd
 import datetime as dt
 
+#TODO: Adicionar um arquivo de log para monitoramento futuro
+
 SCORE_TAG_MEANINGS ={
 'P+': 'P+: strong positive',
 'P': 'P: positive',
@@ -12,11 +14,13 @@ SCORE_TAG_MEANINGS ={
 'NONE':'NONE: without polarity'
 }
 
-def feeling_analysis(text):
+KEY = 'b9a2dc96eb49d8757b7bac4e0995e4f7' #TODO: Adicionar uma camada de seguranca. Talvez um config criptografado.
+
+def feeling_analysis(text: str) -> str:
     url = "https://api.meaningcloud.com/sentiment-2.1"
 
     payload={
-        'key': 'b9a2dc96eb49d8757b7bac4e0995e4f7',
+        'key': KEY,
         'txt': text,
         'lang': 'auto'
     }
@@ -25,7 +29,7 @@ def feeling_analysis(text):
 
     response_status = response.status_code
 
-    if response_status == 200:
+    if response_status == 200: # TODO: Aprimorar o tratamento dos erros
         dict_return = json.loads(response.text)
         return SCORE_TAG_MEANINGS[dict_return['score_tag']]
     else:
@@ -33,9 +37,9 @@ def feeling_analysis(text):
         exit(1)
 
 date = dt.datetime.now().strftime('%Y%m%d')
-parquet_df = pd.read_parquet(f'Heading_News_{date}.parquet')
+parquet_df = pd.read_parquet(f'Raw_News/Raw_News_{date}.parquet')
 
 parquet_df['feeling'] = parquet_df['title'].apply(feeling_analysis)
 
-parquet_df.to_parquet(f'Daily_News_{date}.parquet', index=None)
-parquet_df.to_csv(f'Daily_News_{date}.csv', index=False, sep=';')
+parquet_df.to_parquet(f'Daily_News/Daily_News_{date}.parquet', index=None)
+parquet_df.to_csv(f'Daily_News/Daily_News_{date}.csv', index=False, sep=';')
